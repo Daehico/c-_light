975143c55dd61428c1d6f304cb3e34a1263cb210
Дмитрий <empire_95@mail.ru>
2019-01-26 10:42:17 +0300

    task 2 fix

--- Lesson 2/Task 2/Task 2/Program.cs
+++ Lesson 2/Task 2/Task 2/Program.cs
@@ -11,14 +11,10 @@ namespace Task_2
         static void Main(string[] args)
         {
             Console.Write("Добро пожаловать в ковертер валют! Для обмена доступны 3 валюты. Доллары, евро и биткоин. " +
-                "(Вводить с маленькой буквы и в точности как написано в предложении перед скобками). Введите название валюты:");
-           string name = Console.ReadLine();
-            //if (name != "доллары" || name != "евро" || name != "биткоин")
-            //{
-            //    Console.WriteLine("Вы ввели не корректное название валюты.");
-            //}
-            //else
-            //{
+                "(Вводить в точности как написано в предложении перед скобками. Можно без учета регистра). Введите название валюты:");
+            string name = Console.ReadLine();
+            if (name.ToLower() == "доллары" || name.ToLower() == "биткоин" || name.ToLower() == "евро")
+            {
                 Console.Write("Введите сумму, которую хотите обменять: ");
                 float money = Convert.ToSingle(Console.ReadLine());
                 if (money < 0)
@@ -29,106 +25,59 @@ namespace Task_2
                 {
                     Console.Write("Введите валюту в которую хотите конвертировать: ");
                     string nameConvertation = Console.ReadLine();
-                    //if (nameConvertation != "доллары" || nameConvertation != "евро" || nameConvertation != "биткоин")
-                    //{
-                    //    Console.WriteLine("Вы ввели не корректное название валюты.");
-                    //}
-                    //else
-                    //{
+                    if (nameConvertation.ToLower() == "доллары" || nameConvertation.ToLower() == "биткоин" || nameConvertation.ToLower() == "евро")
+                    {
                         float conversionRate;
                         float result;
-                        if (name == "доллары" && nameConvertation == "евро")
+                        if (name.ToLower() == "доллары" && nameConvertation.ToLower() == "евро")
                         {
                             conversionRate = 1.5f;
                             result = money * conversionRate;
-                            Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
+                            Console.WriteLine("Вы обменяли: " +money.ToString("0.00") + "долларов. Теперь у вас " + result.ToString("0.00") + "евро.");
                         }
-                        else if (name == "доллары" && nameConvertation == "биткоин")
+                        else if (name.ToLower() == "доллары" && nameConvertation.ToLower() == "биткоин")
                         {
                             conversionRate = 0.3f;
                             result = money * conversionRate;
-                            Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
+
+                            Console.WriteLine("Вы обменяли: " + money.ToString("0.00") + "долларов. Теперь у вас " + result.ToString("0.00") + "биткоинов.");
                         }
-                        else if (name == "евро" && nameConvertation == "биткоин")
+                        else if (name.ToLower() == "евро" && nameConvertation.ToLower() == "биткоин")
                         {
                             conversionRate = 0.2f;
                             result = money * conversionRate;
-                            Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
+                            Console.WriteLine("Вы обменяли: " + money.ToString("0.00") + "евро. Теперь у вас " + result.ToString("0.00") + "биткоинов.");
                         }
 
-                        else if (name == "евро" && nameConvertation == "доллар")
+                        else if (name.ToLower() == "евро" && nameConvertation.ToLower() == "доллары")
                         {
                             conversionRate = 2.7f;
                             result = money * conversionRate;
-                            Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
+                            Console.WriteLine("Вы обменяли: " + money.ToString("0.00") + "евро. Теперь у вас " + result.ToString("0.00") + "долларов");
                         }
-                        else if (name == "биткоин" && nameConvertation == "доллар")
+                        else if (name.ToLower() == "биткоин" && nameConvertation.ToLower() == "доллары")
                         {
                             conversionRate = 3600f;
                             result = money * conversionRate;
-                    string stringResult = result.ToString("0.00");
-                            Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, stringResult, nameConvertation);
+                            Console.WriteLine("Вы обменяли: " + money.ToString("0.00") + "биткоинов. Теперь у вас " + result.ToString("0.00") + "долларов.");
                         }
-                        else if (name == "биткоин" && nameConvertation == "евро")
+                        else if (name.ToLower() == "биткоин" && nameConvertation.ToLower() == "евро")
                         {
                             conversionRate = 3800f;
                             result = money * conversionRate;
-                            Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
+                            Console.WriteLine("Вы обменяли: " + money.ToString("0.00") + "биткоинов. Теперь у вас " + result.ToString("0.00") + "евро.");
                         }
                     }
-            Console.ReadKey();
-        }
+                }
+                }
             
+            else
+            {
+                Console.WriteLine("Введенное наименование валюты не корректно");
+            }
+
+            Console.ReadKey();
+          
         }
-           
-
-            //if (money < 0)
-            //{
-            //    Console.WriteLine("Вы ввели не корректную сумма");
-            //}
-            //else if (name != "доллары" || name != "евро" || name != "биткоин" || name != "биткоины" || name != "доллар" /*|| nameConvertation != "доллары" || nameConvertation != "евро" || nameConvertation != "биткоин" || nameConvertation != "биткоины" || nameConvertation != "доллар"*/)
-            //{
-            //    Console.WriteLine("Вы ввели не корректное название валюты.");
-            //} 
-            //else if (name == "доллары" || name == "доллар" && nameConvertation =="евро")
-            //{
-            //    conversionRate = 1.5f;
-            //    result = money * conversionRate;
-            //    Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
-            //}
-            //else if ((name == "доллары" || name == "доллар") && (nameConvertation == "биткоин" || nameConvertation == "биткоины"))
-            //{
-            //    conversionRate = 0.3f;
-            //    result = money * conversionRate;
-            //    Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
-            //}
-            //else if (name == "евро" && nameConvertation == "биткоин" || nameConvertation == "биткоины")
-            //{
-            //    conversionRate = 0.2f;
-            //    result = money * conversionRate;
-            //    Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
-            //}
-
-            //else if (name == "евро" && nameConvertation == "доллар" || nameConvertation == "доллары")
-            //{
-            //    conversionRate = 2.7f;
-            //    result = money * conversionRate;
-            //    Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
-            //}
-            //else if (name == "биткоин" || name == "биткоины" && nameConvertation == "доллар" || nameConvertation == "доллары")
-            //{
-            //    conversionRate = 3600f;
-            //    result = money * conversionRate;
-            //    Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
-            //}
-            //else if (name == "биткоин" || name == "биткоины" && nameConvertation == "евро")
-            //{
-            //    conversionRate = 3800f;
-            //    result = money * conversionRate;
-            //    Console.WriteLine("Вы обменяли {0} {1}. Теперь у вас {2] {3}", money, name, result.ToString("0.00"), nameConvertation);
-            //}
-
-            //Console.ReadKey();
-        }
-    //}
-//}
+    }
+}
