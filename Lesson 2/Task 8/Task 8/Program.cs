5feff0938aa0733d279376cbc13d3b39b9da12f0
Дмитрий <empire_95@mail.ru>
2019-01-26 11:03:46 +0300

    tas2,8 fix

--- Lesson 2/Task 8/Task 8/Program.cs
+++ Lesson 2/Task 8/Task 8/Program.cs
@@ -10,21 +10,22 @@ namespace Task_8
     {
         static void Main(string[] args)
         {
-            Console.Write("Добро пожаловать в программу! Введите пароль, если он совпадет, то вы узнаете секрет! У вас 5 попыток.");
+            Console.WriteLine("Добро пожаловать в программу! Введите пароль, если он совпадет, то вы узнаете секрет! У вас 5 попыток.");
             string ourPassword = "12345";
             string userPassword;
             for (int i = 0; i < 5; i++)
             {
-                Console.Write("Введите пароль");
+                int _try = i;
+                int trys = _try+ 1;
+                Console.Write("Введите пароль:");
                 userPassword = Console.ReadLine();
                 if (userPassword == ourPassword)
                 {
                     Console.WriteLine("Секрет расскрыт!");
                     break;
                 }
-                int _try = i;
-                string trys = _try++.ToString();
-                Console.WriteLine("Это ваша {0} попытка", trys);
+               
+                Console.WriteLine("Это ваша {0} попытка", trys.ToString());
 
             }
             Console.WriteLine("Спасибо за использование программы! Для выхода нажмите любую клавишу");
