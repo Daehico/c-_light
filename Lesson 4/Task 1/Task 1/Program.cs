fee33119b96d9290f193457c6d687282df4e86f7
Дмитрий Дудин <empire_95@mail.ru>
2019-02-03 23:52:44 +0300

    Task 1 fix

--- Lesson 4/Task 1/Task 1/Program.cs
+++ Lesson 4/Task 1/Task 1/Program.cs
@@ -20,8 +20,9 @@ namespace Task_1
             {
                 if (comand.ToLower() == "добавить досье")
                 {
-                    Adddossier(fioArray, positionArray);
                     
+                   fioArray = AdddossierName(fioArray);
+                   positionArray = AddDossierPosition(positionArray);  
                 }
                 if (comand.ToLower() == "вывести все досье")
                 {
@@ -29,7 +30,10 @@ namespace Task_1
                 }
                 if (comand.ToLower() == "удалить досье")
                 {
-                    DeleteDossier(fioArray, positionArray);
+                    Console.Write("Введите ID сотрудника досье которого вы хотите удалить. Для того что бы узнать ID сотруддника ввыведите список сотрудников через команду вывести все досье: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    fioArray = DeleteDossierName(fioArray,id);
+                    positionArray = DeleteDossierPosition(fioArray, id);
                 }
                 Console.Write("Введите команду: ");
                 comand = Console.ReadLine();
@@ -38,46 +42,69 @@ namespace Task_1
             
         }
 
-        static void Adddossier(string[] fioArray,string[] positionArray)
+        static string[] AdddossierName(string[] fioArray)
         {
             string name;
-            string position;
+            
             
             string[] fioArrayCopy;
-            string[] positionArrayCopy;
+            
 
             Console.Write("Введите имя: ");
             name = Console.ReadLine();   
-            Console.Write("Введите должность: ");
-            position = Console.ReadLine();
+           
             
             Array.Copy(fioArray,  fioArrayCopy = new string[fioArray.Length], fioArray.Length);
-            Array.Copy(positionArray, positionArrayCopy = new string[positionArray.Length], positionArray.Length);
-            Array.Resize<string>(ref fioArray, fioArray.Length + 1);
-            Array.Resize<string>(ref positionArray, positionArray.Length + 1);
+            
+            Array.Resize(ref fioArray, fioArray.Length + 1);
+           
             for (int i = 0; i < fioArrayCopy.Length; i++)
             {
                 fioArray[i] = fioArrayCopy[i];
-                positionArray[i] = positionArrayCopy[i];
+                
             }
             fioArray[fioArray.Length - 1] = name;
-            positionArray[positionArray.Length-1] = position;
+           
+            return fioArray;
+        }
+        static string[] AddDossierPosition(string[] positionArray)
+        {
+            string position;
+            string[] positionArrayCopy;
+            Console.Write("Введите должность: ");
+            position = Console.ReadLine();
+            Array.Copy(positionArray, positionArrayCopy = new string[positionArray.Length], positionArray.Length);
+            Array.Resize(ref positionArray, positionArray.Length + 1);
+            for (int i = 0; i < positionArrayCopy.Length; i++)
+            {
+                positionArray[i] = positionArrayCopy[i];
+            }
+            positionArray[positionArray.Length - 1] = position;
+            return positionArray;
         }
 
         static void SeeAllDossier(string[] fioArray, string[] positionArray)
         {
            for (int i = 0; i < fioArray.Length; i++)
             {
-            Console.WriteLine(i + ")" + fioArray[i], positionArray[i]);
+                if (fioArray[i] == null || positionArray[i] == null)
+                    continue;
+                else
+            Console.WriteLine("{0}) ФИО - {1}; Должность - {2} ",i,fioArray[i],positionArray[i]);
             }
         }
 
-        private static void DeleteDossier(string[] fioArray, string[] positionArray)
+       static string[] DeleteDossierName(string[] fioArray, int id)
         {
-            Console.Write("Введите ID сотрудника досье которого вы хотите удалить. Для того что бы узнать ID сотруддника ввыведите список сотрудников через команду вывести все досье: ");
-            int id = Convert.ToInt32(Console.ReadLine());
+           
             Array.Clear(fioArray, id,id);
+            return fioArray;
+           
+        }
+        static string[] DeleteDossierPosition(string[] positionArray,int id)
+        {
             Array.Clear(positionArray, id, id);
+            return positionArray;
         }
 
     }
